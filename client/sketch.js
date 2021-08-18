490f1f9bdb7605bd8fedc7f9d863871afb115cb3
arthur <arthur.massanes@epitech.eu>
2021-08-19 01:35:17 +0200

    First client

---
+++ client/sketch.js
@@ -0,0 +1,17 @@
+
+const gWidth = 600;
+const gHeight = 400;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    fullscreen(true);
+}
+
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+ }
+
+function draw() {
+    background(51);
+}
