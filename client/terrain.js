83cdce559bff13760be76a52bf8621ce85188726
arthur <arthur.massanes@epitech.eu>
2021-08-22 04:06:01 +0200

    more terrain randomization

--- client/terrain.js
+++ client/terrain.js
@@ -17,6 +17,7 @@ class Terrain {
        });      
     }
 
+    // this should be created using server data
     generateObstacles() {
         const options = {
             isStatic: true,
@@ -24,11 +25,11 @@ class Terrain {
             // friction: 0
         };
 
-        for (let i = 0; i < 10; i++) {
-            const height = 30;
-            const width = gWidth / 3;
+        for (let i = 0; i < 3; i++) {
+            const height = random(10, 30);
+            const width = random(gWidth / 4, gWidth / 3);
             const x = random(0, gWidth);
-            const y = random(gHeight / 2, gHeight);
+            const y = random(gHeight / 2, gHeight - this.ground.height);
             const bodyColor = random(200, 255);
             const body = Bodies.rectangle(x, y, width, height, options);
             World.add(world, body);
