a09da606d1e0086cc837a469bc23ef7dc86a22e0
arthur <arthur.massanes@epitech.eu>
2021-08-27 22:09:30 +0200

    wrong url again

--- client/socket.js
+++ client/socket.js
@@ -1,5 +1,5 @@
 
-// const url = "https://a-shooter.herokuapp.com/"
-const url = "http://localhost:3000";
+const url = "https://a-shooter.herokuapp.com/"
+// const url = "http://localhost:3000";
 var socket = io(url);
 
