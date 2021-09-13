42854c5092dbdac4ee3aa9bdd86f40e75fc08c40
arthur <arthur.massanes@epitech.eu>
2021-09-13 03:24:54 +0200

    Join game by url

--- client/src/sketch.js
+++ client/src/sketch.js
@@ -3,8 +3,8 @@ const gHeight = 720;
 const FPS = 30;
 const YGRAVITY = 3;
 
-const url = "https://a-shooter.herokuapp.com/"
-// const url = "http://localhost:3000";
+// const url = "https://a-shooter.herokuapp.com/"
+const url = "http://localhost:3000";
 var socket = io(url);
 
 var mgr;
@@ -14,8 +14,14 @@ function setup() {
     angleMode(RADIANS);  
     createCanvas(gWidth, gHeight);
 
+    // check if joining game
+    let params = getURLParams();
     mgr = new SceneManager();
-    mgr.showScene(Menu);
+    if (params.room) {
+        console.log(params.room);
+        socket.emit("joinRoom", params.room);
+        mgr.showScene(Game)
+    } else mgr.showScene(Menu);
 }
 
 function mousePressed() {
