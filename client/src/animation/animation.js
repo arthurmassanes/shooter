6426c674c7427f7d1a0bea9050024dc880879fb4
arthur <arthur.massanes@epitech.eu>
2021-09-24 02:31:41 +0200

    Load sprites by animation key

--- client/src/animation/animation.js
+++ client/src/animation/animation.js
@@ -4,13 +4,15 @@ const spritesFolders = {
 
 class Animation {
     constructor(spriteFolder = spritesFolders.COWBOY, state = ANIMATION_STATE.WALK) {
-        this.frames = [];
+        this.frames = {
+            WALK: [],
+            JUMP: []
+        };
         this.index = 0;
         // defines which character sprite to use
         this.spriteFolder = spriteFolder;
         
         this.loaded = false;
-        this.json = undefined;
         this.speed = 0.4;
         this.state = state;
         this.isFacingLeft = false;
@@ -19,29 +21,31 @@ class Animation {
     }
     
     async loadSprite(spriteFolder) {
-        // get the frames from spritesheet using json data
-        loadJSON(`${spriteFolder}/data.json`, json => {
-            this.json = json;
-            // load image once we have json data
-            loadImage(`${spriteFolder}/sprite.png`, img => this.generateSpriteSheet(img, this.json));
+        Object.keys(ANIMATION_STATE).map(key => {
+            const animationLabel = ANIMATION_STATE[key];
+            // get the frames from spritesheet using json data
+            loadJSON(`${spriteFolder}/${animationLabel}.json`, json => {
+                // load image once we have json data
+                loadImage(`${spriteFolder}/${animationLabel}.png`, img => this.generateSpriteSheet(img, json, key));
+            });
         });
     }
     
-    generateSpriteSheet(img, json) {
-        this.sprite = img;
-        console.log('loading', json);
+    generateSpriteSheet(img, json, animationKey) {
         json.frames.map(f => {
             const { x, y, w, h } = f.frame;
             const frame = img.get(x, y, w, h);
-            this.frames.push(frame);
+            this.frames[animationKey].push(frame);
         });
         this.loaded = true;
     }
 
     getCurrentFrame() {
-        const animationIndex = Math.abs(round(this.index)) % this.frames.length;
+        const currentAnimation = this.frames[this.state.toUpperCase()];
+        if (!currentAnimation) return;
+        const animationIndex = Math.abs(round(this.index)) % currentAnimation.length;
 
-        return this.frames[animationIndex];
+        return currentAnimation[animationIndex];
     }
     
     draw(pos, angle, vel) {
@@ -54,7 +58,7 @@ class Animation {
         if (this.loaded) {
             const frame = this.getCurrentFrame();
             if (this.isFacingLeft) scale(-1, 1);
-            image(frame, 0, 0);
+            frame && image(frame, 0, 0);
         }
         
         pop();
